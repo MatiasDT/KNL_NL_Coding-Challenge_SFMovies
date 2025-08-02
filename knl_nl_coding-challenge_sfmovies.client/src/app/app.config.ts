0f5816afc202f5d4052e26269a8c82f4ae5bc2c8
Matias Del Toro <matias_dt@outlook.com>
2025-08-02 17:36:14 -0300

    feat: Implement mapbox  sdk

--- knl_nl_coding-challenge_sfmovies.client/src/app/app.config.ts
+++ knl_nl_coding-challenge_sfmovies.client/src/app/app.config.ts
@@ -1,9 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
+import { provideMapboxGL } from 'ngx-mapbox-gl';
+
+import { routes } from './app.routes';
+import { environment } from '../environments/environment';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withFetch()),]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideMapboxGL({
+      accessToken: environment.mapboxToken,
+    }),
+  ],
 };
--- knl_nl_coding-challenge_sfmovies.client/src/app/pages/film-locations-page/film-locations-page.component.ts
+++ knl_nl_coding-challenge_sfmovies.client/src/app/pages/film-locations-page/film-locations-page.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { MapComponent } from 'ngx-mapbox-gl';
 
 @Component({
   selector: 'app-film-locations-page',
-  imports: [],
+  imports: [MapComponent],
   templateUrl: './film-locations-page.component.html',
   styles: `
+    mgl-map {
+          height: 100%;
+          width: 100%;
+        }
   `,
 })
 export class FilmLocationsPageComponent {}
