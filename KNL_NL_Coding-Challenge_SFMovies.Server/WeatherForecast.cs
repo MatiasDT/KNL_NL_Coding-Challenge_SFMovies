7019608f30e9918374e86a5199ab65c160442cf9
Matias Del Toro <matias_dt@outlook.com>
2025-08-02 10:36:08 -0300

    init project

---
+++ KNL_NL_Coding-Challenge_SFMovies.Server/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace KNL_NL_Coding_Challenge_SFMovies.Server
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
