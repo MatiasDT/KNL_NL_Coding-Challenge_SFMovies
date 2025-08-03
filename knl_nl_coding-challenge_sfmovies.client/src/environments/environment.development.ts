1fb8dfa13510a7c64ae7a264aacb1d257e8574a4
Matias Del Toro <matias_dt@outlook.com>
2025-08-03 20:18:40 -0300

    chore: Fix docker deploy

--- knl_nl_coding-challenge_sfmovies.client/src/environments/environment.development.ts
+++ knl_nl_coding-challenge_sfmovies.client/src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  mapboxToken:
-    'pk.eyJ1IjoibWF0aWFzZGVsdG9ybyIsImEiOiJjbWRzejdwaTEwMjJ0MmpwbGx1c2dveG5oIn0.K2jOXyHS-5cAQB3T19oUdQ',
+  mapboxToken: 'pk.XXXX.XXXX',
 };
